3c72a2f370a24615fd2cf124de890b05fe82abcd
Hendrik-A <145358341+Hendrik-A@users.noreply.github.com>
2023-09-20 08:56:06 +0200

    Update run_dancer_generation.sh
    set dataset_name instead of path, set test_batch_size to 2, set max_test_samples to 10

--- run_dancer_generation.sh
+++ run_dancer_generation.sh
@@ -2,11 +2,12 @@ python src/dancer_generation.py \
     --mode dancer \
     --model_path dancer_pubmed/models \
     --output_path dancer_pubmed \
-    --data_path /home/jupyter/pubmed-dataset/processed/pubmed/test.json \
+    --dataset_name Hendrik-a/DANCER_data \
     --text_column document \
     --summary_column summary \
     --write_rouge 1 \
     --seed 100 \
-    --test_batch_size 6 \
+    --test_batch_size 2 \
     --max_source_length 512 --max_summary_length 128 \
     --num_beams 5
+    --max_test_samples 10
